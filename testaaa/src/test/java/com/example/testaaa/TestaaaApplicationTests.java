f9f619ee581f22d987fbd285f4f3e3fb43be61b6
kdhm <test@test.com>
2025-05-09 18:38:17 +0900

    fff

---
+++ testaaa/src/test/java/com/example/testaaa/TestaaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testaaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestaaaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
