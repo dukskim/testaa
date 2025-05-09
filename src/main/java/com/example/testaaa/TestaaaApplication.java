6c8567f87f32d3cf07101892143088fb5aef3022
kdhm <test@test.com>
2025-05-09 18:45:08 +0900

    uuu

---
+++ src/main/java/com/example/testaaa/TestaaaApplication.java
@@ -0,0 +1,13 @@
+package com.example.testaaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestaaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestaaaApplication.class, args);
+	}
+
+}
